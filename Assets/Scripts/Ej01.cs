7d3b623c927e49dd268dd0bb16b0fef135186e69
SashaCattaneo1412 <101577464+SashaCattaneo1412@users.noreply.github.com>
2022-03-29 10:40:28 -0300

    Ex 1 y 2 finished

--- Assets/Scripts/Ej01.cs
+++ Assets/Scripts/Ej01.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Ej01 : MonoBehaviour
 {
+    //Realizá un programa donde se cree una variable entera llamada num1, que inicialmente  valdrá 0. Luego incrementá su valor en 2 y mostralo por pantalla. Después mostrá el  resultado de multiplicarlo por sí mismo. 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int num1 = 0;
+        num1 += 2;
+        Debug.Log("El numero es " + num1);
+        Debug.Log("El numero es " + num1 * num1);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Ej02.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej02 : MonoBehaviour
+{
+    // 2. Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5,  respectivamente, realice la operación correspondiente y muestre el resultado en pantalla: 
+    //a.Suma
+    //b.Producto
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 4;
+        int num2 = 5;
+        Debug.Log("El resultado de la suma es " + num1 + num2);
+        Debug.Log("El resultado del producto es " + num1 * num2);
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
